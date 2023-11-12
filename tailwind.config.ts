8819397d5bdc37e1f96ba3380e13f58ba634fcc9
Tiotedd <andrelsbrito2022@gmail.com>
2023-11-12 19:14:56 -0300

    chore: config something color theme

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -10,9 +10,13 @@ const config: Config = {
     extend: {
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        'target': 'linear-gradient(270deg, #CD0AED 0%, #510AE8 40.1%, #100CE8 100%)'
       },
+      colors: {
+        primary: "#111215",
+        secondary: "#FFFFFF0D",
+      }
     },
   },
   plugins: [],
