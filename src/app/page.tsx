ea0bdc2f447055e370d9bbd505b1df515001c0a0
Tiotedd <andrelsbrito2022@gmail.com>
2023-11-14 20:20:22 -0300

    feat: add components into main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,28 @@
 import Image from 'next/image'
 import { NavBar } from './components/navBar'
 import { Header } from './components/header';
+import { SectionContainer } from './components/sectionContainer';
+import { ServiceCard } from './components/serviceCard';
+import { serviceData } from "./utils/data"
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between bg-primary">
+    <main className="flex min-h-screen flex-col items-center justify-between bg-primary text-white">
       <div className='absolute bg-secondary clip-triangle w-full h-[100vh] top-0'/>
 
       <NavBar />
 
       <Header />
+
+      <SectionContainer
+        id='services'
+        title='O QUE SEI FAZER'
+        subTitle='Tenho paixão em levar a solução através da programação.'
+      >
+       {serviceData.map((service, index) => ( 
+        <ServiceCard/>
+       ))}
+      </SectionContainer>
     </main>
   )
 }
