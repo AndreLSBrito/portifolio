16d7d499626ba1e825b9476b97dc9dd23067b4f0
Tiotedd <andrelsbrito2022@gmail.com>
2023-11-14 16:35:40 -0300

    feat: add navbar button for mobile screens

--- src/app/components/navBar.tsx
+++ src/app/components/navBar.tsx
@@ -1,19 +1,47 @@
+'use client'
+
+import { LuAlignJustify } from 'react-icons/lu'
+import { useState } from 'react'
+import { Button } from '@/components/ui/button'
+
 export function NavBar(){
+  const [navOpen, setNavOpen] = useState(false)
+
   return (
-    <nav className="flex w-full justify-between py-4 items-center">
+    <nav className="flex w-full justify-between py-4 items-center relative px-24">
       <div className="text-center">
         <span className="text-3xl text-neutral-400 text">ANDRÉ LUÍS</span>
         <p className="text-base bg-clip-text text-transparent bg-target">desenvolvedor</p>
       </div>
 
+        <Button 
+          onClick={() => setNavOpen(!navOpen)}
+          className='lg:hidden flex-col bg-secondary hover:bg-secondary/30'  
+        >
+          <LuAlignJustify
+            color="white"
+            size="2rem"
+          />
+
+          {navOpen && (
+            <div className='w-full absolute z-20 bg-indigo-950 flex top-0 left-0 mt-20 justify-center'>
+              <ul className="flex-col text-white">
+                <li className='mt-2'><a href="#home">Home</a></li>
+                <li className='mt-2'><a href="#sobre">Sobre</a></li>
+                <li className='mt-2'><a href="#projetos">Projetos</a></li>
+                <li className='mt-2'><a href="#contatos">Contatos</a></li>
+              </ul>
+            </div>
+          )}
+        </Button>
+
+        <ul className="hidden lg:flex gap-16 ">
+          <li className='mt-2 text-white hover:bg-clip-text hover:text-transparent hover:bg-target'><a href="#home">Home</a></li>
+          <li className='mt-2 text-white hover:bg-clip-text hover:text-transparent hover:bg-target'><a href="#sobre">Sobre</a></li>
+          <li className='mt-2 text-white hover:bg-clip-text hover:text-transparent hover:bg-target'><a href="#projetos">Projetos</a></li>
+          <li className='mt-2 text-white hover:bg-clip-text hover:text-transparent hover:bg-target'><a href="#contatos">Contatos</a></li>
+        </ul>
       
-      <ul className="flex gap-8 text-white">
-        <li><a href="#home">Home</a></li>
-        <li><a href="#sobre">Sobre</a></li>
-        <li><a href="#projetos">Projetos</a></li>
-        <li><a href="#contatos">Contatos</a></li>
-      </ul>
-     
     </nav>
   )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,31 +1,15 @@
 import Image from 'next/image'
 import { NavBar } from './components/navBar'
+import { Header } from './components/header';
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between pt-0 p-24 bg-primary relative z-[-1]">
-        <div className='absolute bg-secondary clip-triangle w-full h-[100vh] z-0 top-0'></div>
-        
-        <section className='w-full h-[100vh]'>
-          <NavBar/>
+    <main className="flex min-h-screen flex-col items-center justify-between bg-primary ">
+      <div className='absolute bg-secondary clip-triangle w-full h-[100vh] top-0'/>
 
-          <div className='flex items-center justify-around'>
-            <figure>
-              <Image
-                src="/next.svg"
-                alt=''
-                width={200}
-                height={200}
-                className='rounded-full border'
-              />
-            </figure>
+      <NavBar />
 
-            <h1 className='w-96'> 
-              olá, Meu nome é André Luís. Sou desenvolvedor. Ajudo você a criar 
-              <span className='text-base bg-clip-text text-transparent bg-target'>Web sites</span>  
-            </h1>
-          </div>
-        </section>
+      <Header />
     </main>
   )
 }
