5ed77e2985b8e1254551c77518947ec49a57c009
Tiotedd <andrelsbrito2022@gmail.com>
2023-11-14 20:19:46 -0300

    feat: add data file

---
+++ src/app/utils/data.ts
@@ -0,0 +1,25 @@
+import { CiMobile4 } from "react-icons/ci";
+
+
+export const serviceData = [
+  {
+    service: "Soluções de Aplicativos Móveis Personalizadas",
+    description: "Quero ajudar a transformar suas ideias em aplicativos móveis de sucesso.",
+    icon: CiMobile4 ,
+  },
+  {
+    service: "Soluções de Aplicativos Móveis Personalizadas",
+    description: "Quero ajudar a transformar suas ideias em aplicativos móveis de sucesso.",
+    icon: CiMobile4 ,
+  },
+  {
+    service: "Soluções de Aplicativos Móveis Personalizadas",
+    description: "Quero ajudar a transformar suas ideias em aplicativos móveis de sucesso.",
+    icon: CiMobile4  ,
+  },
+  {
+    service: "Soluções de Aplicativos Móveis Personalizadas",
+    description: "Quero ajudar a transformar suas ideias em aplicativos móveis de sucesso.",
+    icon: CiMobile4 ,
+  },
+]
