4a6fce7ba8e06616c5efa7df0783039f20453a09
Tiotedd <andrelsbrito2022@gmail.com>
2023-11-14 23:47:05 -0300

    feat: add data to manege page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,8 @@ import { Header } from './components/header';
 import { SectionContainer } from './components/sectionContainer';
 import { ServiceCard } from './components/serviceCard';
 import { serviceData } from "./utils/data"
+import { Separator } from '@/components/ui/separator';
+import { SkillTabs } from './components/skillTabs';
 
 export default function Home() {
   return (
@@ -18,10 +20,34 @@ export default function Home() {
         id='services'
         title='O QUE SEI FAZER'
         subTitle='Tenho paixão em levar a solução através da programação.'
+        className='relative'
       >
-       {serviceData.map((service, index) => ( 
-        <ServiceCard/>
-       ))}
+        {serviceData.map((service, index) => ( 
+        <ServiceCard
+          key={index}
+        />
+        ))}
+
+        <Separator className='bg-target w-[60%] my-20 right-0 absolute top-full'/>
+      </SectionContainer>
+
+      <SectionContainer
+        id='skills'
+        title='Como faço'
+        subTitle='Skills'
+        className='mt-40'
+        slogan='Meu objetivo é encontrar soluções simples e eficientes que atendam as exigências, com o foco sempre no cliente.'
+      >
+        <SkillTabs/>
+      </SectionContainer>
+
+      <SectionContainer
+        id='skills'
+        title='portfólio'
+        subTitle='Meus Projetos e Aplicações Recentes.'
+        className='mt-40'
+      >
+        
       </SectionContainer>
     </main>
   )
--- src/app/utils/data.ts
+++ src/app/utils/data.ts
@@ -23,3 +23,90 @@ export const serviceData = [
     icon: CiMobile4 ,
   },
 ]
+
+export type AreaType = ('front' | 'back' | 'data' | 'design')
+
+
+type SkillDataProps = {
+  image: string;
+  name: string;
+  area: AreaType[]
+}
+
+export const skillsData: SkillDataProps[] = [
+  {
+    image: 'html5/html5-original.svg',
+    name: 'HTML',
+    area: ["front"]
+  },
+  {
+    image: 'css3/css3-original.svg',
+    name: 'CSS',
+    area: ["front"]
+  },
+  {
+    image: 'javascript/javascript-original.svg',
+    name: 'JavaScript',
+    area: ["front", "back"]
+  },
+  {
+    image: 'typescript/typescript-original.svg',
+    name: 'TypeScript',
+    area: ["front", "back"]
+  },
+  {
+    image: 'react/react-original.svg',
+    name: 'React',
+    area: ["front"]
+  },
+  {
+    image: 'tailwindcss/tailwindcss-plain.svg',
+    name: 'Tailwindcss',
+    area: ["front"]
+  },
+  {
+    image: 'nextjs/nextjs-original.svg',
+    name: 'Nextjs',
+    area: ["front"]
+  },
+  {
+    image: 'react/react-original.svg',
+    name: 'React Native',
+    area: ["front"]
+  },
+  {
+    image: 'nodejs/nodejs-original.svg',
+    name: 'NodeJs',
+    area: ["back"]
+  },
+  {
+    image: 'python/python-original.svg',
+    name: 'Python',
+    area: ["data", "back"]
+  },
+  {
+    image: 'microsoftsqlserver/microsoftsqlserver-plain-wordmark.svg',
+    name: 'SQL Server',
+    area: ["data"]
+  },
+  {
+    image: 'postgresql/postgresql-original-wordmark.svg',
+    name: 'PostgreSQL',
+    area: ["data"]
+  },
+  {
+    image: 'figma/figma-original.svg',
+    name: 'Figma',
+    area: ["design"]
+  },
+  {
+    image: 'amazonwebservices/amazonwebservices-original-wordmark.svg',
+    name: 'AWS',
+    area: ["back"]
+  },
+  {
+    image: 'oracle/oracle-original.svg',
+    name: 'Oracle',
+    area: ["data"]
+  },
+]
