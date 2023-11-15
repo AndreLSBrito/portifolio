133e3b5126d8922aeeefc4cef4abf61ab4307633
Tiotedd <andrelsbrito2022@gmail.com>
2023-11-14 23:44:49 -0300

    feat: add SkillTabs component

---
+++ src/app/components/skillTabs.tsx
@@ -0,0 +1,75 @@
+'use client'
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
+import { SkillCard } from "./skillCard"
+import { AreaType, skillsData } from "../utils/data"
+import {useState} from 'react'
+
+
+export function SkillTabs(){
+  const [skill, setSkill] = useState<AreaType | 'all'>("all")
+
+  const skillsSelected = (skill === 'all') 
+    ? skillsData 
+    : skillsData.filter((skillSelected) => ( skillSelected.area.includes(skill)))
+
+  return (
+    <Tabs defaultValue="TODAS" className="w-[400px]">
+      <TabsList className="bg-primary ">
+        <TabsTrigger className="hover:bg-clip-text hover:text-transparent hover:bg-target"  value="TODAS" onClick={()=> setSkill('all')}>TODAS</TabsTrigger>
+        <TabsTrigger className="hover:bg-clip-text hover:text-transparent hover:bg-target" value="DESIGN" onClick={()=> setSkill('design')}>DESIGN</TabsTrigger>
+        <TabsTrigger className="hover:bg-clip-text hover:text-transparent hover:bg-target" value="FRONT-END" onClick={()=> setSkill('front')}>FRONT-END</TabsTrigger>
+        <TabsTrigger className="hover:bg-clip-text hover:text-transparent hover:bg-target" value="BACK-END" onClick={()=> setSkill('back')}>BACK-END</TabsTrigger>
+        <TabsTrigger  className="hover:bg-clip-text hover:text-transparent hover:bg-target"value="DADOS" onClick={()=> setSkill('data')}>DADOS</TabsTrigger>
+      </TabsList>
+      <TabsContent value="TODAS" > 
+        {skillsSelected.map((skill, index) => (
+          <SkillCard
+            key={index}
+            name={skill.name}
+            image={skill.image}
+          />
+        ))} 
+      </TabsContent>
+
+      <TabsContent value="DESIGN" >
+        {skillsSelected.map((skill, index) => (
+            <SkillCard
+              key={index}
+              name={skill.name}
+              image={skill.image}
+            />
+          ))}
+      </TabsContent>
+
+      <TabsContent value="FRONT-END" >
+        {skillsSelected.map((skill, index) => (
+          <SkillCard
+            key={index}
+            name={skill.name}
+            image={skill.image}
+          />
+        ))}
+      </TabsContent>
+
+      <TabsContent value="BACK-END" >
+        {skillsSelected.map((skill, index) => (
+          <SkillCard
+            key={index}
+            name={skill.name}
+            image={skill.image}
+          />
+        ))}
+      </TabsContent>
+
+      <TabsContent value="DADOS" >
+        {skillsSelected.map((skill, index) => (
+          <SkillCard
+            key={index}
+            name={skill.name}
+            image={skill.image}
+          />
+        ))}
+      </TabsContent>
+    </Tabs>
+  )
+}
