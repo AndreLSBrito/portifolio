daa87ac02397ddd8583e2af9aca64793bb7b4a21
Tiotedd <andrelsbrito2022@gmail.com>
2023-11-14 23:46:07 -0300

    feat: add SkillCard component

---
+++ src/app/components/skillCard.tsx
@@ -0,0 +1,21 @@
+import Image from "next/image"
+
+interface SkillCardProps {
+  name: string;
+  image: string
+}
+
+export function SkillCard({name, image}: SkillCardProps) {
+  return (
+    <div className="flex flex-col justify-center items-center gap-4 bg-secondary rounded-md px-4 py-8 mt-4">
+      <Image
+         src={`https://cdn.jsdelivr.net/gh/devicons/devicon/icons/${image}`}
+        alt={name}
+        width={40}
+        height={40}
+      />
+      
+      <p>{name}</p>
+    </div>
+  )
+}
