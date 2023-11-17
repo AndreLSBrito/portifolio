d882b828bc86e741cf53ecb277f7ce597a9f77a3
Tiotedd <andrelsbrito2022@gmail.com>
2023-11-17 20:30:40 -0300

    feat: add dynamic icons component with lucid react

--- next.config.js
+++ next.config.js
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'i.ibb.co',
+        port: '',
+        pathname: '/**/**'
+      }
+    ]
+  },
+  transpilePackages: ['lucide-react'] 
+}
 
 module.exports = nextConfig
