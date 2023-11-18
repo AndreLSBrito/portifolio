16bd82eb57df0b941f2331a9ad3e0885d7285dad
Tiotedd <andrelsbrito2022@gmail.com>
2023-11-17 22:28:37 -0300

    style: ensure best experience for mobiles devices

--- src/app/components/projectCard.tsx
+++ src/app/components/projectCard.tsx
@@ -4,7 +4,7 @@ import { projectType } from "../utils/data";
 export function ProjectCard({name, description, image, repo, tags, url, stack}: projectType){
   return (
     <a href={url} target="_blank">
-      <div className="h-96 flex flex-col overflow-hidden relative rounded-lg justify-start items-center gap-6 w-[400px] bg-secondary px-4 py-8 mt-4 hover:h-full hover:duration-500 hover:scale-105 transition-all ease-in-out duration-500">
+      <div className="h-full flex flex-col overflow-hidden relative rounded-lg justify-start items-center gap-6 w-[400px] bg-secondary px-4 py-8 mt-4 hover:duration-500 hover:scale-105 transition-all ease-in-out duration-500">
         <div className="absolute text-xs left-[80%] top-3 bg-target text-primary p-1 rounded-full">{stack}</div>
         
         <h3>{name}</h3>
@@ -16,7 +16,8 @@ export function ProjectCard({name, description, image, repo, tags, url, stack}: 
           alt={name}
           width={200}
           height={300}
-          className="w-full rounded-sm h-full"
+          className="w-auto rounded-sm h-auto"
+          style={{objectFit: "contain"}}
         />
 
         <div className=" gap-2 flex-wrap text-xs flex mt-6 justify-start items-start">
