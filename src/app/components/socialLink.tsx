f1c1af12776b1cdca1269ebccf852b7908609e72
Tiotedd <andrelsbrito2022@gmail.com>
2023-11-20 15:53:10 -0300

    style: change stroke color icons

--- src/app/components/socialLink.tsx
+++ src/app/components/socialLink.tsx
@@ -1,5 +1,6 @@
 import Link from "next/link";
 import { Icon, IconProps } from "./icon";
+import { Gradient } from "./gradient";
 
 export interface SocialLinkProps extends IconProps {
   link: string;
@@ -8,14 +9,18 @@ export interface SocialLinkProps extends IconProps {
 export function SocialLink({iconName, link}: SocialLinkProps){
  
   return (
+    <>
+    <Gradient />
+    
     <Link href={link}>
       <div className="flex items-center justify-center border rounded-full w-10 h-10 " >
         <Icon 
           iconName={iconName}
           strokeWidth={1}
-          color="#E9ED30"
+          style={{stroke: "url(#gradient"}}  
         />  
       </div>
     </Link>
+    </>
   )
 }
