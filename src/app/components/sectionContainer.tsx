e382b3cc7f017ab1f75727cb0d24f39418e45c45
Tiotedd <andrelsbrito2022@gmail.com>
2023-11-20 15:44:25 -0300

    style: adjust text size for desktop devices

--- src/app/components/sectionContainer.tsx
+++ src/app/components/sectionContainer.tsx
@@ -10,11 +10,11 @@ interface SectionContainerProps extends HTMLProps<HTMLDivElement> {
 export function SectionContainer({title, subTitle, slogan, children, className, ...props}: SectionContainerProps){
   return (
     <section className={` px-16 flex flex-col items-center text-center mt-20 gap-4 ${className}`} {...props}>
-      <h4 className="font-light text-base uppercase  tracking-[7px]">{title}</h4>
+      <h4 className="font-light text-base uppercase lg:text-lg tracking-[7px]">{title}</h4>
   
-      <h2 className="font-medium text-3xl bg-clip-text text-transparent bg-target ">{subTitle}</h2>
+      <h2 className="font-medium max-w-4xl text-3xl lg:text-5xl bg-clip-text text-transparent bg-target ">{subTitle}</h2>
 
-      {slogan && <h3 className='text-neutral-400 text-sm mb-20'>{slogan}</h3>}
+      {slogan && <h3 className='text-neutral-400 max-w-2xl text-sm mb-20'>{slogan}</h3>}
 
       {children}
     </section>
